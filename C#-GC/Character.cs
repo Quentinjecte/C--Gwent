3e105e472ba68bb3e3a797be314dcde74ec3ba54
deadly <vgauter@gaming.tech>
2024-03-18 09:14:27 +0100

    first chara commit

---
+++ C#-GC/Character.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__GC
+{
+    struct Stats
+    {
+        int atk;
+        int hp;
+        int mana;
+    }
+
+
+    internal class  Offense
+    {
+        public int dmg;
+
+        public virtual void effect() { }
+    }
+
+
+    internal class Spell
+    {
+        enum Type
+        {
+            demonic,
+            holy,
+            sanity,
+            shadow
+        }
+
+        public int dmg;
+        public int type;
+
+        public virtual void effect() { }
+    }
+
+
+    internal class Character
+    {
+        public Stats stats;
+        public string name;
+        public Offense[] offenses;
+        public Spell[] spells;
+    }
+}
