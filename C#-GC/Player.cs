ccb179de7dbc672de4d6817bb85bdea67a7c0927
Riuck01 <tforest@gaming.tech>
2024-03-18 11:11:26 +0100

    Move marche ? lol

--- C#-GC/Player.cs
+++ C#-GC/Player.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace C__GC
 {
+
     struct Inventory
     {
         int golds;
@@ -15,8 +16,70 @@ namespace C__GC
 
     internal class Player
     {
+        enum MoveDirection
+        {
+            Forward,
+            Backward,
+            Left,
+            Right
+        }
+
+        public int playerX = 0;
+        public int playerY = 0;
+
+
+        public static class Keyboard;
         public Protagonist[] team;
         public Inventory inventory;
         //save
+
+        public void Input()
+        {
+            MoveDirection moveDirection;
+
+            do
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.Z:
+                        moveDirection = MoveDirection.Forward;
+                        break;
+                    case ConsoleKey.S:
+                        moveDirection = MoveDirection.Backward;
+                        break;
+                    case ConsoleKey.Q:
+                        moveDirection = MoveDirection.Left;
+                        break;
+                    case ConsoleKey.D:
+                        moveDirection = MoveDirection.Right;
+                        break;
+                    default:
+                        continue;
+                }
+
+                Move(moveDirection);
+
+            } while (true);
+        }
+        private void Move(MoveDirection direction)
+        {
+            switch (direction)
+            {
+                case MoveDirection.Forward:
+                    playerY--;
+                    break;
+                case MoveDirection.Backward:
+                    playerY++;
+                    break;
+                case MoveDirection.Left:
+                    playerX--;
+                    break;
+                case MoveDirection.Right:
+                    playerX++;
+                    break;
+            }
+        }
     }
 }
