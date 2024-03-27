6ffe99ed152adbf2ebc5378833c13c6bc745ad64
Riuck01 <tforest@gaming.tech>
2024-03-27 20:09:40 +0100

    dialogue de base

---
+++ C#-GC/Dialogue.cs
@@ -0,0 +1,22 @@
+﻿using C__GC.DataString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__GC
+{
+    internal class Dialogue
+    {
+        DisplayElement renderer;
+        public void Read()
+        {
+            renderer = new DisplayElement("Bien le bonjour c'est jerem", 15, 0, 0);
+
+            DisplaySystem.Subscribe(renderer);
+        }
+
+
+    }
+}
