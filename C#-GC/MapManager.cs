ddf3c322a290e922367e6bb11f044012dfd9a22c
Quentinjecte <qrenaud@gaming.tech>
2024-03-28 22:08:48 +0100

    .

---
+++ C#-GC/MapManager.cs
@@ -0,0 +1,69 @@
+﻿using C__GC.DataString;
+using C__GC.Entity;
+using C__GC.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__GC
+{
+    public class MapManager
+    {
+        private ResourceAllocator _allocator;
+        private Player.Player _player;
+
+        internal MapManager(ResourceAllocator allocator)
+        {
+            _allocator = allocator;
+            _player = new();
+        }
+        public void StartMap() 
+        {
+
+            // Get the initial map named "map1" from the ResourceAllocator
+            //string initialMap = _allocator.GetBackMap("map1");
+            string initialMap = _allocator.GetFrontMap("mapMask.txt", 48);
+            // Add FrontMap
+
+            if (initialMap == null)
+            {
+                Console.WriteLine("Map 'map1' not found.");
+                return;
+            }
+
+            // Set the content of the map display
+            DisplayElement map = new DisplayElement(initialMap, 983, 0, 0);
+            // Update the display
+            DisplaySystem.Subscribe(map);
+            DisplaySystem.Update();
+
+            // Initialize player after loading maps successfully
+            _player.InitPlayer(initialMap, 100, this);
+            _player.Input(0, 0);
+        }
+
+        public string ChangeMap(string mapName)
+        {
+
+            string newMap = _allocator.GetBackMap(mapName);
+            // Add FrontMap
+
+            if (newMap == null)
+            {
+                throw new Exception("failed to load map");
+            }
+
+            DisplayElement mapDisplay = new DisplayElement(newMap, 100, 0, 0);
+
+            // Subscribe the map to the DisplaySystem
+            DisplaySystem.ReplaceByIndex(0, mapDisplay);
+            DisplaySystem.Update();
+
+            return newMap;
+        }
+    }
+
+}
---
+++ C#-GC/RessourceAllocator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class ResourceAllocator
+{
+    public Dictionary<string, string> _mapName;
+
+    public ResourceAllocator()
+    {
+        _mapName = new Dictionary<string, string>();
+    }
+
+    // Get map data by name
+    public string GetBackMap(string mapName)
+    {
+        // Get the directory where the executable is located
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        // Path to the "maps.json" file in the same directory as the executable
+        string mapsJsonPath = Path.Combine(baseDirectory, "../../../maps/maps.json");
+
+        if (_mapName.ContainsKey(mapName))
+        {
+            return _mapName[mapName];
+        }
+        else
+        {
+            try {
+                string content = File.ReadAllText(mapsJsonPath);
+                _mapName = JsonSerializer.Deserialize<Dictionary<string, string>>(content);
+            }
+            catch (FileNotFoundException)
+            {
+                    Console.WriteLine("File not found.");
+             }
+            catch (Exception)
+            {
+                    Console.WriteLine("An error occurred while loading maps");
+             }
+            return _mapName[mapName];
+        }
+    }
+
+    public string GetFrontMap(string mapName, int width)
+    {
+        if (_mapName.ContainsKey(mapName))
+        {
+            return _mapName[mapName];
+        }
+        else
+        {
+            using (StreamReader map = new StreamReader("../../../maps/"+mapName))
+            {
+                int count = 0;
+                int startIndex = -1;
+                string str = map.ReadToEnd();
+                //while ((startIndex = str.IndexOf("\\e", startIndex + 1)) != -1)
+                //{
+                //    str = str.Insert(startIndex, "<BR>");
+                //}
+                str = str.Replace("\\e", "\x1b");
+                map.Close();
+                _mapName.Add(mapName, str);
+            }
+            return _mapName[mapName];
+        }
+    }
+}
